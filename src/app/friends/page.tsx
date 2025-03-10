0e9eb8b6e9ef97d39ac285bf960857de370209d7
RejectModders <142326052+RejectModders@users.noreply.github.com>
2025-03-10 08:17:56 -0500

    Update page.tsx

--- src/app/friends/page.tsx
+++ src/app/friends/page.tsx
@@ -1,11 +1,9 @@
 import { type Metadata } from 'next'
 import { SimpleLayout } from '@/components/layout/SimpleLayout'
 
-import { friendsHeadLine, friendsIntro, projects, githubProjects, friends } from '@/config/infoConfig'
+import { friendsHeadLine, friendsIntro, friends } from '@/config/infoConfig'
 
 import { FriendCard } from '@/components/friends/FriendCard'
-import { GithubProjectCard } from '@/components/project/GithubProjectCard'
-import { CustomIcon } from '@/components/shared/CustomIcon'
 
 export const metadata: Metadata = {
   title: 'Friends',
@@ -18,14 +16,16 @@ export default function Friends() {
       title={friendsHeadLine}
       intro={friendsIntro}
     >
-      <ul
-        role="list"
-        className="grid grid-cols-1 gap-x-8 gap-y-12 sm:grid-cols-2 lg:grid-cols-3 pb-10"
-      >
-        {friends.map((friend) => (
-          <FriendCard key={friend.name} friend={friend} />
-        ))}
-      </ul>
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8 mt-16 sm:mt-32">
+        <ul
+          role="list"
+          className="grid grid-cols-1 gap-x-8 gap-y-12 sm:grid-cols-2 lg:grid-cols-3 pb-10"
+        >
+          {friends.map((friend) => (
+            <FriendCard key={friend.name} friend={friend} />
+          ))}
+        </ul>
+      </div>
     </SimpleLayout>
   )
 }
