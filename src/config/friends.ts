1982a0c6037df1d7ecfc792cf491ede95b63fd39
RejectModders <142326052+RejectModders@users.noreply.github.com>
2025-03-11 00:39:53 -0500

    Update friends.ts

--- src/config/friends.ts
+++ src/config/friends.ts
@@ -2,7 +2,6 @@
 export const friendsHeadLine = "My closest friends!"
 export const friendsIntro = "Meet some interesting friends."
 
-
 // friends
 export type FriendItemType = {
   name: string
@@ -15,18 +14,16 @@ export const friends: Array<FriendItemType> = [
   {
     name: 'FeralHS',
     link: { href: 'https://discord.com/users/935404512691224586' },
-    logo: 'https://cdn.is-it.pink/u/FeralHS.png' 
+    logo: '/images/friends/FeralHS.png' 
   },
   {
     name: 'weebUHD',
     link: { href: 'https://discord.com/users/402921518515879937' },
-    logo: 'https://cdn.is-it.pink/u/weebUHD.jpg'
+    logo: '/images/friends/weebUHD.jpg'
   },
   {
     name: 'Jiggly Balls',
     link: { href: 'https://discord.com/users/1022085572719808542' },
-    logo: 'https://cdn.is-it.pink/u/jiggly-balls.jpg'
+    logo: '/images/friends/jiggly-balls.jpg'
   },
 ]
-
-
