da6f27530a600fb7e824d4f806cccdf74f7d644b
RejectModders <142326052+RejectModders@users.noreply.github.com>
2025-03-12 10:08:55 -0500

    Update projects.ts

--- src/config/projects.ts
+++ src/config/projects.ts
@@ -68,7 +68,7 @@ export type ProjectItemType = {
   export const githubProjects: Array<ProjectItemType> = [
     {
       name: 'disckit',
-      description: 'An open source utilities library for the disutils bots.',
+      description: 'An open source utilities library for the disutils bots',
       link: { href: 'github.com/disutils/disckit', label: 'disckit' },
       gitStars: 1,
       gitForks: 0
@@ -76,15 +76,15 @@ export type ProjectItemType = {
     {
       name: 'Is It Pink?',
       description:
-        'A kids game I made for the fun of it.',
-      link: { href: 'github.com/disutils/is-it.pink', label: 'Is It Pink?' },
+        'A kids game I made for the fun of it',
+      link: { href: 'github.com/disutils/is-it.pink', label: 'Is It Pink' },
       gitStars: 0,
       gitForks: 0
     },
     {
       name: 'Discord.py-Template',
       description:
-        'A Discord.py template made so others know how to make discord bots in python.',
+        'A Discord.py template made so others know how to make discord bots in python',
       link: { href: 'github.com/RejectModders/Discord.py-Template', label: 'Discord.py-Template' },
       gitStars: 1,
       gitForks: 1
